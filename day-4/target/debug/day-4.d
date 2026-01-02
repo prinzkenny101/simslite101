d15cfb83bcf258a6738c175f10b1463c3f972fb5
prinzkenny101 <kennedy.ekeozo@pau.edu.ng>
2026-01-02 11:41:17 +0100

    Day 4 Sketch

---
+++ day-4/target/debug/day-4.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\simslite101\day-4\target\debug\day-4.exe: C:\Users\New\Documents\simslite101\day-4\src\main.rs
---
+++ day-4/target/debug/deps/day_4-585d0a035c17b4d1.d
@@ -0,0 +1,5 @@
+C:\Users\New\Documents\simslite101\day-4\target\debug\deps\day_4-585d0a035c17b4d1.d: src\main.rs
+
+C:\Users\New\Documents\simslite101\day-4\target\debug\deps\day_4-585d0a035c17b4d1.exe: src\main.rs
+
+src\main.rs:
